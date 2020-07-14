5bce3e5e6897f3650a200a940ec5733455086f22
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-14 10:12:47 -0700

    changes to old linked list

--- queue/linked_list.py
+++ queue/linked_list.py
@@ -1,113 +1,98 @@
-
 class Node:
-    def __init__(self, value):
+
+    def __init__(self, value=None, next_node=None):
         self.value = value
-        self.next = None
+        self.next_node = next_node
 
     def get_value(self):
         return self.value
 
     def get_next(self):
-        return self.next
+        return self.next_node
 
     def set_next(self, new_next):
-        self.next = new_next
+        self.next_node = new_next
+
 
 class LinkedList:
+
     def __init__(self):
         self.head = None
-        self.tail = None 
+        self.tail = None
 
     def add_to_tail(self, value):
-        # 1. create the Node from the value 
-        new_node = Node(value)
-        # So, what do we do if tail is None? 
-        # What's the rule we want to set to indicate that the linked
-        # list is empty? 
-        # Would it be better to check the head? 
-        # Let's check them both: an empty linked list has an empty 
-        # head and an empty tail 
-        if self.head is None and self.tail is None:
-            # in a one-element linked list, what should head and tail 
-            # be referring to? 
-            # have both head and tail referring to the single node 
+        new_node = Node(value, None)
+        if not self.head:
+            
             self.head = new_node
-            # set the new node to be the tail 
             self.tail = new_node
+     
         else:
-            # These steps assume that the tail is already referring
-            # to a Node 
-            # 2. set the old tail's next to refer to the new Node 
+     
             self.tail.set_next(new_node)
-            # 3. reassign self.tail to refer to the new Node 
             self.tail = new_node
 
     def remove_head(self):
-        # if we have an empty linked list 
-        if self.head is None and self.tail is None:
-            return
-        # what if we only have a single elem in the linked list?
-        # both head and tail are pointing at the same Node 
+
+        if not self.head:
+            return None
+
         if not self.head.get_next():
-            head = self.head 
-            # delete the linked list's head reference 
+            head = self.head
             self.head = None
-            # also delete the linked list's tail reference 
-            self.tail = None 
+            self.tail = None
             return head.get_value()
-        val = self.head.get_value()
-        # set self.head to the Node after the head 
+       
+        value = self.head.get_value()
+       
         self.head = self.head.get_next()
-        return val
+        return value
 
     def remove_tail(self):
-        # if we have an empty linked list 
-        if self.head is None and self.tail is None:
-            return
-        # if we have a non-empty linked list 
-        # we have to start at the head and move down the linked list 
-        # until we get to the node right before the tail 
-        # iterate over our linked list 
-        current = self.head 
+        if not self.head:
+            return None
+
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+
+        current = self.head
 
         while current.get_next() is not self.tail:
             current = current.get_next()
-        # at this point, `current` is the node right before the tail 
-        # set the tail to be None
-        val = self.tail.get_value() 
-        # move self.tail to the Node right before
+            
+        value = self.tail.get_value()
         self.tail = current
-        return val
+        self.tail.set_next(None)
+        return value
 
     def contains(self, value):
         if not self.head:
             return False
-    
-        # get a reference to the node we're currently at; update this as we traverse the list
+
         current = self.head
-        # check to see if we're at a valid node 
+      
         while current:
-            # return True if the current value we're looking at matches our target value
+           
             if current.get_value() == value:
                 return True
-            # update our current node to the current node's next node
+
             current = current.get_next()
-        # if we've gotten here, then the target node isn't in our list
+      
         return False
 
-    # def get_max(self):
-    #     if not self.head:
-    #         return None
-    #     # reference to the largest value we've seen so far
-    #     max_value = self.head.get_value()
-    #     # # reference to our current node as we traverse the list
-    #     current = self.head.get_next()
-    #     # # check to see if we're still at a valid list node
-    #     while current:
-    #     #     # check to see if the current value is greater than the max_value
-    #         if current.get_value() > max_value:
-    #     #         # if so, update our max_value variable
-    #             max_value = current.get_value()
-    #     #     # update the current node to the next node in the list
-    #         current = current.get_next() 
-    #         return max_value
+    def get_max(self):
+        if not self.head:
+            return None
+
+        max_value = self.head.get_value()
+        current = self.head.get_next()
+
+        while current:
+            if current.get_value() > max_value:
+                max_value = current.get_value()
+
+            current = current.get_next()
+        return max_value
